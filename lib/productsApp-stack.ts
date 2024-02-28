44c5a75475a761f4a16c02a013fcff1fc92f4cb4
João Macieira <jp.macieira@uol.com.br>
2024-02-28 19:28:54 -0300

    feat: create products admin stack

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -6,6 +6,7 @@ import { Construct } from "constructs";
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+  readonly productsAdminHandler: lambdaNodeJs.NodejsFunction;
   readonly productsDdb: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -48,5 +49,26 @@ export class ProductsAppStack extends cdk.Stack {
     );
 
     this.productsDdb.grantReadData(this.productsFetchHandler);
+
+    this.productsAdminHandler = new lambdaNodeJs.NodejsFunction(
+      this,
+      "ProductsAdminFunction",
+      {
+        functionName: "ProductsAdminFunction",
+        entry: "lambda/products/productsAdminFunction.ts",
+        handler: "handler",
+        memorySize: 512,
+        runtime: lambda.Runtime.NODEJS_20_X,
+        timeout: cdk.Duration.seconds(5),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName
+        }
+      }
+    );
+    this.productsDdb.grantWriteData(this.productsAdminHandler);
   }
 }
