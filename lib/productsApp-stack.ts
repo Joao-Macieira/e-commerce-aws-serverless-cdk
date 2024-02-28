cbc0c142aecd717c91148a20198d1161ac1773b9
João Macieira <jp.macieira@uol.com.br>
2024-02-28 19:26:20 -0300

    feat: create dynamodb products table stack

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,13 +1,31 @@
 import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+  readonly productsDdb: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.productsDdb = new dynamodb.Table(
+      this,
+      "ProductsDdb",
+      {
+        tableName: "Products",
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        partitionKey: {
+          name: "id",
+          type: dynamodb.AttributeType.STRING,
+        },
+        billingMode: dynamodb.BillingMode.PROVISIONED,
+        readCapacity: 1,
+        writeCapacity: 1,
+      }
+    );
 
     this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(
       this,
@@ -23,7 +41,12 @@ export class ProductsAppStack extends cdk.Stack {
           minify: true,
           sourceMap: false,
         },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName
+        }
       }
-    )
+    );
+
+    this.productsDdb.grantReadData(this.productsFetchHandler);
   }
 }
